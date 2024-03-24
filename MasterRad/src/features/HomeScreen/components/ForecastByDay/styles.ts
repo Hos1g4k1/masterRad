b9bc7e99ade64d98edd719226ecc0f9a60cc9c6e
H0s1g4k1 <lazar.celikovic@fishingbooker.com>
2024-03-24 23:23:34 +0100

    [WIP] Add future days forecast

---
+++ MasterRad/src/features/HomeScreen/components/ForecastByDay/styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  listItemStyle: {
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+});
+
+export default styles;
