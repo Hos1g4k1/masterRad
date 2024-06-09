871e294bfe1ac26f97fb28165a801a8b7d3e6488
H0s1g4k1 <lazar.celikovic@fishingbooker.com>
2024-06-09 14:19:14 +0200

    [WIP] Add error boudary

--- MasterRad/App.tsx
+++ MasterRad/App.tsx
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux';
 import store from './src/store';
 import NavigationComponent from './src/navigation';
 import * as Sentry from '@sentry/react-native';
+import Text from './src/components/Text';
 
 const routingInstrumentation = new Sentry.ReactNavigationInstrumentation({
   enableTimeToInitialDisplay: true,
@@ -14,6 +15,7 @@ Sentry.init({
   dsn: 'https://626ccbd77d09e9a1949cf8029d1a015b@o4506208597180416.ingest.us.sentry.io/4506208611729408',
   integrations: [new Sentry.ReactNativeTracing({routingInstrumentation})],
   tracesSampleRate: 1,
+  enableAutoPerformanceTracing: true,
   _experiments: {
     profilesSampleRate: 1.0,
   },
@@ -22,9 +24,14 @@ Sentry.init({
 function App(): JSX.Element {
   return (
     <Provider store={store}>
-      <SafeAreaView style={{flex: 1}}>
-        <NavigationComponent routingInstrumentation={routingInstrumentation} />
-      </SafeAreaView>
+      <Sentry.ErrorBoundary
+        fallback={<Text variation="body1">An error has occurred</Text>}>
+        <SafeAreaView style={{flex: 1}}>
+          <NavigationComponent
+            routingInstrumentation={routingInstrumentation}
+          />
+        </SafeAreaView>
+      </Sentry.ErrorBoundary>
     </Provider>
   );
 }
